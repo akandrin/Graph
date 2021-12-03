cf68f7e64d6906704d1f5f527f0d35c7e5550e19
mrleha01 <adkandrin@gmail.com>
2021-12-03 19:33:38 +0300

    Add comments to tests.

--- Pavel_Lab4/test_graph.cpp
+++ Pavel_Lab4/test_graph.cpp
@@ -141,11 +141,13 @@ namespace
 	}
 }
 
+// Тест: Может создать граф
 TEST(Graph, can_create_graph)
 {
 	ASSERT_NO_THROW(IntGraph());
 }
 
+// Тест: может построить корректный граф
 TEST(Graph, can_build_correct_graph)
 {
 	// Используется граф 1.
@@ -160,6 +162,7 @@ TEST(Graph, can_build_correct_graph)
 	}));
 }
 
+// Тест: Кидает исключение при попытке построения некорректного графа
 TEST(Graph, throws_when_build_incorrect_graph)
 {
 	StringGraph aGraph;
@@ -167,6 +170,7 @@ TEST(Graph, throws_when_build_incorrect_graph)
 	// Граф не содержит узел "e", но "e" содержится в списке дуг.
 }
 
+// Тест: может добавить узел в пустой граф
 TEST(Graph, can_add_node_to_empty_graph)
 {
 	StringGraph aGraph;
@@ -177,12 +181,14 @@ TEST(Graph, can_add_node_to_empty_graph)
 		}));
 }
 
+// Тест: Добавление узла в пустой граф возвращает истину
 TEST(Graph, adding_node_to_empty_graph_returns_true)
 {
 	StringGraph aGraph;
 	ASSERT_TRUE(aGraph.AddNode("a"));
 }
 
+// Тест: Может добавить узел, которого нет в графе
 TEST(Graph, can_add_node_that_is_not_in_the_graph)
 {
 	// Используется граф 1.
@@ -199,6 +205,7 @@ TEST(Graph, can_add_node_that_is_not_in_the_graph)
 		}));
 }
 
+// Тест: Добавление узла, которого нет в графе, возвращает истину
 TEST(Graph, adding_node_to_graph_that_does_not_have_this_node_returns_true)
 {
 	// Используется граф 1.
@@ -207,6 +214,7 @@ TEST(Graph, adding_node_to_graph_that_does_not_have_this_node_returns_true)
 	ASSERT_TRUE(aGraph.AddNode("e"));
 }
 
+// Тест: Не может добавить узел, который есть в графе
 TEST(Graph, can_not_add_node_that_is_not_in_the_graph)
 {
 	// Используется граф 1.
@@ -222,6 +230,7 @@ TEST(Graph, can_not_add_node_that_is_not_in_the_graph)
 		}));
 }
 
+// Тест: Добавление узла, которого нет в графе, возвращает ложь.
 TEST(Graph, adding_node_to_graph_that_contains_this_node_returns_false)
 {
 	// Используется граф 1.
@@ -230,6 +239,7 @@ TEST(Graph, adding_node_to_graph_that_contains_this_node_returns_false)
 	ASSERT_FALSE(aGraph.AddNode("a"));
 }
 
+// Тест: Может добавить дугу к узлу, который не имеет смежных узлов.
 TEST(Graph, can_add_edge_to_node_that_does_not_have_adjacent_nodes)
 {
 	// Используется граф 1.
@@ -245,6 +255,7 @@ TEST(Graph, can_add_edge_to_node_that_does_not_have_adjacent_nodes)
 		}));
 }
 
+// Тест: Добавление дуги к узлу, который не имеет смежных узлов, возвращает истину.
 TEST(Graph, adding_edge_to_node_that_does_not_have_adjacent_nodes_returns_true)
 {
 	// Используется граф 1.
@@ -253,6 +264,7 @@ TEST(Graph, adding_edge_to_node_that_does_not_have_adjacent_nodes_returns_true)
 	ASSERT_TRUE(aGraph.AddEdge({"c", "a"}));
 }
 
+// Тест: Может добавить дугу к узлу, который уже имеет смежные узлы.
 TEST(Graph, can_add_edge_to_node_that_already_has_adjacent_nodes)
 {
 	// Используется граф 1.
@@ -268,7 +280,8 @@ TEST(Graph, can_add_edge_to_node_that_already_has_adjacent_nodes)
 		}));
 }
 
-TEST(Graph, adding_edge_to_graph_that_does_not_have_this_node_returns_true)
+// Тест: Добавление дуги к графу, который не содержит эту дугу, возвращает истину.
+TEST(Graph, adding_edge_to_graph_that_does_not_have_this_edge_returns_true)
 {
 	// Используется граф 1.
 	StringGraph aGraph;
@@ -276,6 +289,7 @@ TEST(Graph, adding_edge_to_graph_that_does_not_have_this_node_returns_true)
 	ASSERT_TRUE(aGraph.AddEdge({ "b", "a" }));
 }
 
+// Тест: Не может добавить дугу, если она уже есть в графе.
 TEST(Graph, can_not_add_edge_that_is_already_in_the_graph)
 {
 	// Используется граф 1.
@@ -291,6 +305,7 @@ TEST(Graph, can_not_add_edge_that_is_already_in_the_graph)
 		}));
 }
 
+// Тест: Добавление дуги, если она уже есть в графе, возвращает ложь.
 TEST(Graph, adding_edge_that_is_already_in_the_graph_returns_false)
 {
 	// Используется граф 1.
@@ -299,6 +314,7 @@ TEST(Graph, adding_edge_that_is_already_in_the_graph_returns_false)
 	ASSERT_FALSE(aGraph.AddEdge({ "a", "b" }));
 }
 
+// Тест: Кидает исключение при добавлении некорректной дуги.
 TEST(Graph, throws_when_add_incorrect_edge)
 {
 	// Используется граф 1.
@@ -307,6 +323,7 @@ TEST(Graph, throws_when_add_incorrect_edge)
 	ASSERT_ANY_THROW(aGraph.AddEdge({ "a", "e" }));
 }
 
+// Тест: Графы, построенные разными способами, одинаковы.
 TEST(Graph, graphs_constructed_in_different_ways_are_the_same)
 {
 	// Граф 1 строится разными способами
@@ -327,6 +344,7 @@ TEST(Graph, graphs_constructed_in_different_ways_are_the_same)
 	ASSERT_EQ(aGraph1, aGraph2);
 }
 
+// Тест: Может очистить граф.
 TEST(Graph, can_clear_graph)
 {
 	// Используется граф 1.
@@ -336,6 +354,7 @@ TEST(Graph, can_clear_graph)
 	ASSERT_EQ(aGraph, StringGraph());
 }
 
+// Тест: Может удалить узел, который не имеет смежных узлов.
 TEST(Graph, can_remove_node_that_has_no_adjacent_nodes)
 {
 	// Используется граф 1.
@@ -349,6 +368,7 @@ TEST(Graph, can_remove_node_that_has_no_adjacent_nodes)
 	ASSERT_EQ(aGraph1, aGraph2);
 }
 
+// Тест: Может удалить узел, который имеет входящие связи.
 TEST(Graph, can_remove_node_that_has_incomming_connections)
 {
 	// Используется граф 1.
@@ -364,6 +384,7 @@ TEST(Graph, can_remove_node_that_has_incomming_connections)
 		}));
 }
 
+// Тест: Может удалить узел, который имеет исходящие связи.
 TEST(Graph, can_remove_node_that_has_outgoing_connections)
 {
 	// Используется граф 1.
@@ -379,6 +400,7 @@ TEST(Graph, can_remove_node_that_has_outgoing_connections)
 		}));
 }
 
+// Тест: Может удалить узел, который имеет входящие и исходящие связи.
 TEST(Graph, can_remove_node_that_has_incomming_and_outgoing_connections)
 {
 	// Используется граф 1.
@@ -394,6 +416,16 @@ TEST(Graph, can_remove_node_that_has_incomming_and_outgoing_connections)
 		}));
 }
 
+// Тест: Удаление узла, у которого нет смежных узлов, возвращает истину.
+TEST(Graph, removal_node_that_has_no_adjacent_nodes_returns_true)
+{
+	// Используется граф 2.
+	StringGraph aGraph;
+	aGraph.Build({ "a", "b", "c", "d", "e" }, { {"a", "b"}, {"b", "c"}, {"b", "d"}, {"d", "a"} });
+	ASSERT_TRUE(aGraph.RemoveNode("e"));
+}
+
+// Тест: Удаление узла, который имеет входящие связи, возвращает истину.
 TEST(Graph, removal_of_node_that_has_incomming_connections_returs_true)
 {
 	// Используется граф 1.
@@ -402,7 +434,8 @@ TEST(Graph, removal_of_node_that_has_incomming_connections_returs_true)
 	ASSERT_TRUE(aGraph.RemoveNode("c"));
 }
 
-TEST(Graph, removal_of_node_that_has_outgoing_connections)
+// Тест: Удаление узла, который имеет исходящие связи, возвращает истину.
+TEST(Graph, removal_of_node_that_has_outgoing_connections_returns_true)
 {
 	// Используется граф 1.
 	StringGraph aGraph;
@@ -410,7 +443,8 @@ TEST(Graph, removal_of_node_that_has_outgoing_connections)
 	ASSERT_TRUE(aGraph.RemoveNode("a"));
 }
 
-TEST(Graph, removal_of_node_that_has_incomming_and_outgoing_connections)
+// Тест: Удаление узла, который имеет входящие и исходящие связи, возвращает истину.
+TEST(Graph, removal_of_node_that_has_incomming_and_outgoing_connections_returns_true)
 {
 	// Используется граф 1.
 	StringGraph aGraph;
@@ -418,6 +452,7 @@ TEST(Graph, removal_of_node_that_has_incomming_and_outgoing_connections)
 	ASSERT_TRUE(aGraph.RemoveNode("b"));
 }
 
+// Тест: Не может удалить узел, которого нет в графе.
 TEST(Graph, can_not_remove_node_that_is_not_contained_in_graph)
 {
 	// Используется граф 1.
@@ -434,6 +469,7 @@ TEST(Graph, can_not_remove_node_that_is_not_contained_in_graph)
 		}));
 }
 
+// Тест: Удаление узла, которого нет в графе, возвращает ложь.
 TEST(Graph, removal_of_node_that_is_not_contained_in_graph_returns_false)
 {
 	// Используется граф 1.
@@ -442,6 +478,7 @@ TEST(Graph, removal_of_node_that_is_not_contained_in_graph_returns_false)
 	ASSERT_FALSE(aGraph.RemoveNode("e"));
 }
 
+// Тест: Может удалить дугу, которая содержится в графе.
 TEST(Graph, can_remove_edge_that_is_contained_in_graph)
 {
 	// Используется граф 1.
@@ -458,6 +495,7 @@ TEST(Graph, can_remove_edge_that_is_contained_in_graph)
 		}));
 }
 
+// Тест: Не может удалить дугу, которая не содержится в графе.
 TEST(Graph, can_not_remove_edge_that_is_not_contained_in_graph)
 {
 	// Используется граф 1.
@@ -474,6 +512,7 @@ TEST(Graph, can_not_remove_edge_that_is_not_contained_in_graph)
 		}));
 }
 
+// Тест: Не может удалить некорректную дугу.
 TEST(Graph, can_not_remove_incorrect_edge)
 {
 	// Используется граф 1.
@@ -490,6 +529,7 @@ TEST(Graph, can_not_remove_incorrect_edge)
 		}));
 }
 
+// Тест: Удаление дуги, которая есть в графе, возвращает истину.
 TEST(Graph, removal_of_edge_that_is_contained_in_graph_returns_true)
 {
 	// Используется граф 1.
@@ -498,6 +538,7 @@ TEST(Graph, removal_of_edge_that_is_contained_in_graph_returns_true)
 	ASSERT_TRUE(aGraph.RemoveEdge({ "b", "c" }));
 }
 
+// Тест: Удаление дуги, которой нет в графе, возвращает ложь.
 TEST(Graph, removal_of_edge_that_is_not_contained_in_graph_retunrs_false)
 {
 	// Используется граф 1.
@@ -506,7 +547,8 @@ TEST(Graph, removal_of_edge_that_is_not_contained_in_graph_retunrs_false)
 	ASSERT_FALSE(aGraph.RemoveEdge({ "a", "d" }));
 }
 
-TEST(Graph, removal_of_incorrect_edge)
+// Тест: Удаление некорректной дуги возвращает ложь.
+TEST(Graph, removal_of_incorrect_edge_returns_false)
 {
 	// Используется граф 1.
 	StringGraph aGraph;
@@ -514,6 +556,7 @@ TEST(Graph, removal_of_incorrect_edge)
 	ASSERT_FALSE(aGraph.RemoveEdge({ "a", "e" }));
 }
 
+// Тест: Может вывести граф.
 TEST(Graph, can_output_graph)
 {
 	// Используется граф 1.
@@ -522,6 +565,7 @@ TEST(Graph, can_output_graph)
 	ASSERT_EQ(GetInfo(aGraph), "a : { b }\nb : { c, d }\nc : { }\nd : { a }\n");
 }
 
+// Тест: Сравнение равных графов возвращает истину.
 TEST(Graph, compare_equal_graphs_returns_true)
 {
 	// Используется граф 1.
@@ -534,6 +578,7 @@ TEST(Graph, compare_equal_graphs_returns_true)
 	ASSERT_TRUE(aGraph1 == aGraph2 && !(aGraph1 != aGraph2));
 }
 
+// Тест: Сравнение разных графов возвращает ложь.
 TEST(Graph, compare_different_graphs_returns_false)
 {
 	// Используется граф 1.
@@ -546,6 +591,7 @@ TEST(Graph, compare_different_graphs_returns_false)
 	ASSERT_FALSE(aGraph1 == aGraph2 || !(aGraph1 != aGraph2));
 }
 
+// Тест: ExistsPathBetweenTwoNodes возвращает истину для смежных узлов, если существует дуга между ними.
 TEST(Graph, exists_path_between_two_nodes_returns_true_for_adjacent_nodes_if_exists_edge_between_them)
 {
 	// Используется граф 1.
@@ -557,6 +603,7 @@ TEST(Graph, exists_path_between_two_nodes_returns_true_for_adjacent_nodes_if_exi
 		aGraph.ExistsPathBetweenTwoNodes("d", "a"));
 }
 
+// Тест: ExistsPathBetweenTwoNodes возвращает истину для несмежных узлов, если существует путь между ними.
 TEST(Graph, exists_path_between_two_nodes_returns_true_for_non_adjacent_nodes_if_exists_path_between_them)
 {
 	// Используется граф 1.
@@ -568,6 +615,7 @@ TEST(Graph, exists_path_between_two_nodes_returns_true_for_non_adjacent_nodes_if
 		aGraph.ExistsPathBetweenTwoNodes("b", "a"));
 }
 
+// Тест: ExistsPathBetweenTwoNodes возвращает истину для петли.
 TEST(Graph, exists_path_between_two_nodes_returns_true_for_loop)
 {
 	// Используется граф 3.
@@ -577,6 +625,7 @@ TEST(Graph, exists_path_between_two_nodes_returns_true_for_loop)
 		aGraph.ExistsPathBetweenTwoNodes("e", "e"));
 }
 
+// Тест: ExistsPathBetweenTwoNodes возвращает ложь, если пути не существует.
 TEST(Graph, exists_path_between_two_nodes_returns_false_if_path_does_not_exist)
 {
 	// Используется граф 4.
@@ -594,7 +643,8 @@ TEST(Graph, exists_path_between_two_nodes_returns_false_if_path_does_not_exist)
 		!aGraph.ExistsPathBetweenTwoNodes("f", "f"));
 }
 
-TEST(Graph, exists_path_between_two_nodes_throws_when_graph_does_not_contain_fist_of_given_nodes)
+// Тест: ExistsPathBetweenTwoNodes кидает исключение, если граф не содержит первый из переданных узлов.
+TEST(Graph, exists_path_between_two_nodes_throws_when_graph_does_not_contain_first_of_given_nodes)
 {
 	// Используется граф 1.
 	StringGraph aGraph;
@@ -602,6 +652,7 @@ TEST(Graph, exists_path_between_two_nodes_throws_when_graph_does_not_contain_fis
 	ASSERT_ANY_THROW(aGraph.ExistsPathBetweenTwoNodes("e", "a"));
 }
 
+// Тест: ExistsPathBetweenTwoNodes кидает исключение, если граф не содержит второй из переданных узлов.
 TEST(Graph, exists_path_between_two_nodes_throws_when_graph_does_not_contain_second_of_given_nodes)
 {
 	// Используется граф 1.
@@ -610,6 +661,7 @@ TEST(Graph, exists_path_between_two_nodes_throws_when_graph_does_not_contain_sec
 	ASSERT_ANY_THROW(aGraph.ExistsPathBetweenTwoNodes("a", "e"));
 }
 
+// Тест: ExistsPathBetweenTwoNodes кидает исключение, если граф не содержит оба переданных узла.
 TEST(Graph, exists_path_between_two_nodes_throws_when_graph_does_not_contain_both_of_given_nodes)
 {
 	// Используется граф 1.
@@ -618,12 +670,14 @@ TEST(Graph, exists_path_between_two_nodes_throws_when_graph_does_not_contain_bot
 	ASSERT_ANY_THROW(aGraph.ExistsPathBetweenTwoNodes("e", "e"));
 }
 
+// Тест: IsGraphConnected возвращает ложь для пустых графов.
 TEST(Graph, is_graph_connected_returns_false_for_empty_graph)
 {
 	StringGraph aGraph;
 	ASSERT_FALSE(aGraph.IsGraphConnected());
 }
 
+// Тест: IsGraphConnected возвращает истину для графов, состоящих из одного узла.
 TEST(Graph, is_graph_connected_returns_true_for_graph_that_contains_one_node)
 {
 	// Используется граф 6.
@@ -632,6 +686,7 @@ TEST(Graph, is_graph_connected_returns_true_for_graph_that_contains_one_node)
 	ASSERT_TRUE(aGraph.IsGraphConnected());
 }
 
+// Тест: IsGraphConnected возвращаетистину для связных графов.
 TEST(Graph, is_graph_connected_returns_true_for_connected_graph)
 {
 	// Используется граф 5.
@@ -641,6 +696,7 @@ TEST(Graph, is_graph_connected_returns_true_for_connected_graph)
 	ASSERT_TRUE(aGraph.IsGraphConnected());
 }
 
+// Тест: IsGraphConnected возвращает ложь для несвязных графов.
 TEST(Graph, is_graph_connected_returns_false_for_non_connected_graph)
 {
 	// Используется граф 1.
